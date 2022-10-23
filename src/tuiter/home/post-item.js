3f83fb672e5a9d1d75fd3fa46c55829dac874438
Orangemoon8 <62192912+Orangemoon8@users.noreply.github.com>
2022-10-23 15:30:40 -0700

    start challenge for home

---
+++ src/tuiter/home/post-item.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+const PostItem = (
+    post = {
+        "avatarIcon": "starship.jpg",
+        "username": "Elon Musk",
+        "handle": "elonmusk",
+        "time": "23h",
+        "text": "Amazing show about @inspiration4x mission!",
+        "postImage": "NASA.jpg",
+        "url": "netflix.com",
+        "comment_num": "4.2K",
+        "share_num": "3.5K",
+        "like_num": "37.5K",
+        "title": "Countdown: Inspiration4 Mission to Space | Netflix Official Site",
+        "post-content": "From training to launch to landing, this all-access docuseries rides along with the Inspiration4 crew on the first all-civilian orbital space ..."
+    }
+) => {
+
+    return(
+        <div >
+
+        </div>
+    );
+};
+
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ExploreComponent from "./explore";
+import PostList from "./home";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
 import {Routes, Route} from "react-router";
@@ -15,6 +16,7 @@ function Tuiter() {
             <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
                 <ExploreComponent/>
+                <PostList/>
             </div>
             <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
