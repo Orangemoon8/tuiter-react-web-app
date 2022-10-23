80fe17aa86ab4476419e00e4adbda40f1891ab7e
Orangemoon8 <62192912+Orangemoon8@users.noreply.github.com>
2022-10-23 15:00:33 -0700

    finish tuit and continue on challenging for home component

---
+++ src/tuiter/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import postsArray from './posts.json';
+import PostItem from "./post-item";
+
+const HomeComponent = () => {
+    return (
+        <div className={"list-group"} >
+            {
+                postsArray.map(post => <PostItem key={post._id} post={post}/>)
+            }
+        </div>
+    )
+}
+
+export default HomeComponent;
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,27 +1,26 @@
 import React from "react";
+import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
-import PostSummaryList from "./post-summary-list";
-import {Route, Routes} from "react-router";
-let x =6;
+import {Routes, Route} from "react-router";
+import HomeComponent from "./home";
+
 
 function Tuiter() {
     return (
-        <div className={"row mt-2"}>
-            <div className="col-2 col-md-2 col-lg-2 col-xl-2">
-                <NavigationSidebar/>
+        <div className="row mt-2">
+            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                <NavigationSidebar active="explore"/>
             </div>
-
-            <div className="col-10 col-md-10 col-lg-7 col-xl-7" style={{position: "relative"}}>
-                <NavigationSidebar/>
-                <PostSummaryList/>
+            <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                 style={{"position": "relative"}}>
+                <ExploreComponent/>
             </div>
-
-            <div className="col-lg-3 col-xl-3 d-lg-block d-sm-none d-md-none">
+            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
             </div>
         </div>
-    )
+    );
 }
 
 export default Tuiter
