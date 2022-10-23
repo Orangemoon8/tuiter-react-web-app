f1a7d2d96b13e3e4f4ee6947843b1ad49049e67f
Orangemoon8 <62192912+Orangemoon8@users.noreply.github.com>
2022-10-23 13:53:01 -0700

    doing tuit

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,12 +1,25 @@
-import Nav from "../nav";
+import React from "react";
 import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList from "./who-to-follow-list";
+import PostSummaryList from "./post-summary-list";
+import {Route, Routes} from "react-router";
+let x =6;
 
 function Tuiter() {
     return (
-        <div>
-            <Nav/>
-            <NavigationSidebar active="home"/>
-            <h1>Tuiter</h1>
+        <div className={"row mt-2"}>
+            <div className="col-2 col-md-2 col-lg-2 col-xl-2">
+                <NavigationSidebar/>
+            </div>
+
+            <div className="col-10 col-md-10 col-lg-7 col-xl-7" style={{position: "relative"}}>
+                <NavigationSidebar/>
+                <PostSummaryList/>
+            </div>
+
+            <div className="col-lg-3 col-xl-3 d-lg-block d-sm-none d-md-none">
+                <WhoToFollowList/>
+            </div>
         </div>
     )
 }
