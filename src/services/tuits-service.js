b385533052ab3f23cf7f7964a631f7e50685c34c
Orangemoon8 <62192912+Orangemoon8@users.noreply.github.com>
2022-11-21 00:40:33 -0800

    almost finish

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';            // location of HTTP services
 // const TUITS_API = 'https://tuiter-node-server-app886.herokuapp.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE;                  // fix deploy issue
 const TUITS_API = `${API_BASE}/tuits`;
 
 
